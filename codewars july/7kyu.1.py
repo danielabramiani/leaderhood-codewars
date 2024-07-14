3a872cea6c65c0b038d9493942deda915f212fe3
daniel <151828006+danielabramiani@users.noreply.github.com>
2024-07-14 16:21:15 +0400

    Add files via upload

---
+++ codewars july/7kyu.1.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/python
+
+def is_anagram(test, original):
+    if test != original:
+        return True
+    else:
+        return False
